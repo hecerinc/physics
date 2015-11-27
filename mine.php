fd2342651363d364523f66f5528b39d76118b352
Hector Rincon <hecerinc@hotmail.com>
2015-11-26 21:35:34 -0600

    Cleaned up code from last version.

--- mine.php
+++ mine.php
@@ -8,15 +8,13 @@
 	<style>#assets{display: none;}</style>
 </head>
 <body>
-<button style="margin-left:15%; margin-top:20%; position:relative; z-index:10000;">Stahp</button>
+<button style="margin-left:15%; margin-top:20%; position:relative; z-index:10000; width:200px; height:40px;">Stahp</button>
 <div id="assets">
 	<?= file_get_contents('coil.svg'); ?>
 </div>
 <script>
 
 	var two = new Two({
-		// width: 600, l
-		// height:600,
 		type: Two.Types['svg'],
 		fullscreen: true,
 		autostart: true
@@ -43,35 +41,46 @@
 		delta += 20;
 	});
 
+	// Move the whole scene center
 	two.scene.translation.set(two.width / 2, two.height / 2);
 
 	// Path vertices
-	var vertices = [
+	/*var vertices = [
 		[0,0],
 		[100,0],
 		[120,80],
 		[-20,8]
-	];
+	];*/
 
-	// Coil
-	// var easing = 0.125;
-	// var shape = two.interpret($('svg')[0]).center();
-	// shape.visible = true;
-	// shape.scale = .5;
+	// Coil --------------------------------------------
+	/*var easing = 0.125;
+	var coil = two.interpret($('svg')[0]).center();
+	coil.visible = true;
+	coil.scale = .5;*/
 
+	// Create electrong group
 	var electrones = new Two.Group();
 	electrones.add(electrons);
 	two.add(electrones); // Add to scene
+
+
 	two.bind('update', function(frameCount){
 		// return;
-		// console.log(frameCount);
 		// var number = frameCount;
 
-		// console.log(e.translation.y);
-		loopNo = frameCount%321;
-		for(var i=0; i<electrons.length;i++){
-			var e = electrons[i];
-			// izq a derecha
+		// loopNo = frameCount%321;
+
+
+	//	-----------------------------------
+	//				Electron flow
+	//	-----------------------------------
+		var e;
+		
+		for(var i=0; i<electrons.length; i++){
+
+			e = electrons[i];
+
+			// -------->
 			if(e.translation.y == -40){
 				if(e.translation.x != 150)
 					e.translation.x += 2;
@@ -80,47 +89,27 @@
 					e.translation.y += 4;
 				}
 			}
-			// hacia abajo
-			else if(e.translation.x >= 150 && e.translation.x <= 190 && e.translation.y < 120){
-				e.translation.x += 1;
-				e.translation.y += 4;
-			}
 			// <---------
 			else if(e.translation.y >= 120 && e.translation.x != -90){
 				e.translation.x -= 2;
 			}
-			// hacia arriba
+			// down
+			else if(e.translation.x >= 150 && e.translation.x <= 190 && e.translation.y < 120){
+				e.translation.x += 1;
+				e.translation.y += 4;
+			}
+			// up
 			else if(e.translation.y > -40 && e.translation.x >= -90 && e.translation.x <= -50){
 				e.translation.x += 1;
 				e.translation.y -= 4;
 			}
 		}
-		// if(e)
 
-		// for (var i = 0; i < electrons.length; i++) {
-			// if(loopNo > 0 && loopNo <= 40){
-			// 	electrons[0].translation.x += 1;
-			// 	electrons[0].translation.y -= 4;
-			// }
-			// if(loopNo > 40 && loopNo <= 140){ // 100 loopNos para moverte los 200 pxs
-			// 	electrons[0].translation.x += 2;
-			// }
-			// if (loopNo > 140 && loopNo <= 180){
-			// 	electrons[0].translation.x += 1;
-			// 	electrons[0].translation.y += 4;
-			// }
-			// if(loopNo > 180 && loopNo <= 321){
-			// 	electrons[0].translation.x -= 2;
-			// }
-		// };
-		// ._map(electrons, function(electron){
-		// });
-		// shape.rotation += 0.9;
-		// var number = Math.sin(2*Math.PI*(frameCount-60)/100);
-		// var osc = Math.sin(- frameCount / (Math.PI * 10));
-		// electron.translation.x = frameCount-60 /100;
-		// electron.translation.y =  (number?number<0?-1:1:0)*10;
+		// coil.rotation += 0.9;
+
 	}).play();
+
+
 	$(function(){
 		$('button').click(function(e){
 			e.preventDefault();
